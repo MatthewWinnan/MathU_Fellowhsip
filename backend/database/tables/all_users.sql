77d877294ed2e0960a8652d8aad2f94c2274e82b
NongSK <88384279+NongSK@users.noreply.github.com>
2021-08-05 17:24:00 +0200

    Add files via upload

--- backend/database/tables/all_users.sql
+++ backend/database/tables/all_users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Aug 05, 2021 at 01:16 PM
--- Server version: 8.0.21
--- PHP Version: 7.3.21
+-- Host: 127.0.0.1
+-- Generation Time: Aug 05, 2021 at 05:20 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,13 +27,31 @@ SET time_zone = "+00:00";
 -- Table structure for table `all_users`
 --
 
-DROP TABLE IF EXISTS `all_users`;
-CREATE TABLE IF NOT EXISTS `all_users` (
-  `Id` int NOT NULL,
-  `User_id` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Email_address` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `all_users` (
+  `Id` int(11) NOT NULL,
+  `User_ID` varchar(225) NOT NULL,
+  `Email_address` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `all_users`
+--
+ALTER TABLE `all_users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `all_users`
+--
+ALTER TABLE `all_users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
