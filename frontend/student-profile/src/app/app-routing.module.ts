4226fcb6c7dc5dadde7ffd9f3c6be0d634a9f1fc
Matthew <u18037497@tuks.co.za>
2021-08-06 14:28:03 +0200

    Simple routing example done. Sort of get it.

--- frontend/student-profile/src/app/app-routing.module.ts
+++ frontend/student-profile/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { StudentBasicComponent } from './student-basic/student-basic.component';
 import { StudentSubjectsComponent } from './student-subjects/student-subjects.component';
 import { StudentEducationComponent } from './student-education/student-education.component';
 
 const appRoutes: Routes = [
-  { path: 'student-basic', component: StudentBasicComponent },
   { path: 'student-education', component: StudentSubjectsComponent },
   { path: 'student-subjects', component: StudentEducationComponent },
   { path: '',   redirectTo: '/student-basic', pathMatch: 'full' },
--- frontend/student-profile/src/app/app.module.ts
+++ frontend/student-profile/src/app/app.module.ts
@@ -6,22 +6,22 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { StudentBasicComponent } from './student-basic/student-basic.component';
 import { StudentSubjectsComponent } from './student-subjects/student-subjects.component';
 import { StudentEducationComponent } from './student-education/student-education.component';
+import { StudentBasicModule } from './student-basic/student-basic.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentBasicComponent,
     StudentSubjectsComponent,
     StudentEducationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StudentBasicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
