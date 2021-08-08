a8dc2d8026248f98d1b9eca88cde25a3d2d03a65
vraagakrishna <vraagakrishna@gmail.com>
2021-08-08 21:41:17 +0200

    Added a new project with ionic and some edits in Sponsor
    I was trying to figure our the menu in the Sponsor project. The menu works in the iionicMathU folder.

--- frontend/sponsor/src/app/components/common/menu/menu.component.ts
+++ frontend/sponsor/src/app/components/common/menu/menu.component.ts
@@ -1,59 +1,15 @@
-import { Component, OnChanges, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
-  @Output() public sidenavToggle = new EventEmitter();
+  constructor() { }
 
-  // userRoles: UserTypeEnum[] = [];
-  // currentUserRole: UserTypeEnum;
-  // jobSeeker = UserTypeEnum.JOB_SEEKER;
-  // recruiter = UserTypeEnum.RECRUITER;
-  // admin = UserTypeEnum.ADMIN;
-  // superAdmin = UserTypeEnum.SUPER_ADMIN;
-  // default = UserTypeEnum.DEFAULT;
-
-  constructor() {
-    console.log(this.sidenavToggle);
-
+  ngOnInit(): void {
   }
 
-  // constructor(private loginUser: Login) {
-  //   this.currentUserRole = this.default;
-  //   this.userRoles = this.loginUser.userType;
-
-  // }
-
-  // ngOnInit(): void {
-  //   console.log(this.loginUser);
-  //   this.loginUser.loginEventEmitter.subscribe(
-  //       b => {
-  //         console.log(this.loginUser.userType);
-  //         this.userRoles = this.loginUser.userType;
-  //         this.currentUserRole = this.loginUser.currentUserType;
-  //       }
-  //   );
-  // }
-
-  onToggleSidenav(): void {
-    console.log(this.sidenavToggle);
-    this.sidenavToggle.emit();
-  }
-
-  // logout(): void {
-  //   this.loginUser.logout();
-  // }
-
-  // switchUserRole(): void {
-  //   if (this.currentUserRole === this.jobSeeker) {
-  //     this.currentUserRole = this.recruiter;
-  //   }
-  //   else if (this.currentUserRole === this.recruiter) {
-  //     this.currentUserRole = this.jobSeeker;
-  //   }
-  // }
 }
