4d18bbca51b8e725af8ab04f2bb22a65545b36e3
BenisMulenda <benismulenda54@gmail.com>
2021-08-09 23:44:13 +0200

    Update math_u_addSponsor.php

--- backend/Coding Files/Integrated Back & Frontend/math_u_addSponsor.php
+++ backend/Coding Files/Integrated Back & Frontend/math_u_addSponsor.php
@@ -14,88 +14,80 @@ include 'math_u_registration.php';
 
 $input = file_get_contents('php://input');
 $data = json_decode($input, true);
-
+$message = array();
 //---------------
 //Company Section
 //---------------
 
 
-if (isset($data['company_name'])and $data['company_industry'] != "null" and isset($data["email"])){
-	//-------------------------------//
-	$co_name = $data['company_name'];
-	$industry = $data['company_industry'];
-	//------------------------------//
-	//INSERT NEW COMPANY INTO THE DB & GET THE INSERT ID
-	$email = $data['email_address'];
-	$email = strtolower($email);
-	$isUnique = isUniqueEmail($email,$mysqli);
-	//---------------------------------------------------
-	//------------------------//
-	//CHECK EMAIL IS UNIQUE
-	//------------------------//
-	if ($isUnique === TRUE){
-		$company_id = addCompany($co_name, $industry, $mysqli);
-		
-		if ($company_id > 0){
-			//-------------------
-			//Super Admin Section
-			//-------------------
-			if (isset($data['f_name'])and isset($data['l_name']) and isset($data['password'])){
-				//------------------------//
-				$f_name = $data['f_name'];
-				$l_name = $data['l_name'];
-				$pass = $data['password'];
 
-				//INSERT INTO SPONSOR_USERS 
-				$hashed_password = GenerateHashPassword($pass);
-				$spr_id = addSponsor($f_name, $l_name, $email,$hashed_password, $company_id, $mysqli);
-				//--------------------------------------------------------------------
-				if ($spr_id != 0){
-					//GENERATE SPONSOR ID 
-					$new_id = GenerateUserID($spr_id,0);
-					
-					//UPDATE SPONSOR ID 
-					//--------------------------------------------------------
-					if (UpdateID($new_id, $spr_id, $mysqli)===TRUE){
-						
-						//Add to All_USERS TABLE
-						//-------------------------------------------------
-						if (AddToUsers($email, $new_id, $mysqli)===TRUE){
-							echo "<h1>Success! New Admin Inserted. </h1>";
-							echo json_encode(display($spr_id, $mysqli));
-						}
-						else{
-							echo "Could not add Account to AllUsers";
-						}
-						//-------------------------------------------------
-					}
-					else {
-						echo "Could not update sponsor ID!";
-					}
-					//---------------------------------------------------------
+//-------------------------------//
+$co_name = $data['company_name'];
+$industry = $data['company_industry'];
+//------------------------------//
+//INSERT NEW COMPANY INTO THE DB & GET THE INSERT ID
+$email = $data['email_address'];
+$email = strtolower($email);
+$isUnique = isUniqueEmail($email,$mysqli);
+//---------------------------------------------------
+//------------------------//
+//CHECK EMAIL IS UNIQUE
+//------------------------//
+if ($isUnique === TRUE){
+	$company_id = addCompany($co_name, $industry, $mysqli);
+	
+	if ($company_id > 0){
+		//-------------------
+		//Super Admin Section
+		//-------------------
+		$f_name = $data['f_name'];
+		$l_name = $data['l_name'];
+		$pass = $data['password'];
+
+		//INSERT INTO SPONSOR_USERS 
+		$hashed_password = GenerateHashPassword($pass);
+		$spr_id = addSponsor($f_name, $l_name, $email,$hashed_password, $company_id, $mysqli);
+		//--------------------------------------------------------------------
+		if ($spr_id != 0){
+			//GENERATE SPONSOR ID 
+			$new_id = GenerateUserID($spr_id,0);
+			
+			//UPDATE SPONSOR ID 
+			//--------------------------------------------------------
+			if (UpdateID($new_id, $spr_id, $mysqli)===TRUE){
+				
+				//Add to All_USERS TABLE
+				//-------------------------------------------------
+				if (AddToUsers($email, $new_id, $mysqli)===TRUE){
+					//echo "<h1>Success! New Admin Inserted. </h1>";
+					//echo json_encode(display($spr_id, $mysqli));
+					echo json_encode("Success!");
 					
 				}
 				else{
-					echo "Could not Add Admin!";
+					echo json_encode("Could not add Account to AllUsers");
 				}
-			//-------------------------------------------------------------------------	
+				//-------------------------------------------------
 			}
-			else{
-				echo "Please Provide the correct details for the Super Admin!";
+			else {
+				echo json_encode("Could not update sponsor ID!");
 			}
-		
+			//---------------------------------------------------------
+			
 		}
 		else{
-			echo "Could not Insert New Company Try Again!";
+			echo json_encode("Could not Add Admin!");
 		}
+		//-------------------------------------------------------------------------	
 	}
 	else{
-		echo "Email not unique!";
-	}	
+		echo json_encode("Could not Insert New Company Try Again!");
+	}
 }
 else{
-	echo "Form is incomplete!";
-}
+	echo json_encode("Email not unique!");
+}	
+
 
 //==========================END===============================================//
 //==============================
