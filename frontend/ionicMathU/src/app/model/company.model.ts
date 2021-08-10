79509d7984cdc8b22b5d19ad95817b7edb3f79ef
vraagakrishna <vraagakrishna@gmail.com>
2021-08-10 14:20:06 +0200

    register WIP

---
+++ frontend/ionicMathU/src/app/model/company.model.ts
@@ -0,0 +1,13 @@
+export class company {
+    company_id = 0;
+    company_name = "";
+    comapny_logo? : string;
+    company_industry = "";
+    company_description? : string;
+    company_URL? : string; 
+    date_created = "";
+    number_of_reports = "";
+
+}
+
+//registerSponsor()
--- frontend/ionicMathU/src/app/service/api.service.ts
+++ frontend/ionicMathU/src/app/service/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { sponsor_users } from '../model/sponsor_users.model';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,9 +17,8 @@ export class ApiService {
     this.headers.append("Access-Control-Allow-Origin", "http://localhost:8100");
    }
 
-  registerSponsor(data){
+  registerSponsor(data : sponsor_users) : Observable<sponsor_users> {
     //console.log(data);
-    return this.http.post('http://localhost/mathU_backend/math_u_addSponsor.php', data);
-
+    return this.http.post<sponsor_users>('http://localhost/mathU_backend/math_u_addSponsor.php', data);
   }
 }
