eeb56d9d96816b208b70bbe33fe28cf3e6af3aff
NongSK <88384279+NongSK@users.noreply.github.com>
2021-08-11 15:01:21 +0200

    Add files via upload

---
+++ backend/Coding Files/NewEmployee/validate_employee.php
@@ -0,0 +1,200 @@
+<?php
+
+//=================================================================================
+// Connection
+//==============================================================================================================================[[[[[[[[[Commets for Benis Here]]]]]]]]]==================
+
+// include("math_u_registration.php");
+if(isset($_POST['submit'])) {
+
+$first_name = $_POST['fname'];
+$last_name = $_POST['lname'];
+$email = $_POST['email'];
+$sponsor_id = $_POST['sponsor_id'];
+
+if(filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) {
+  exit("Invalid Email");
+}
+
+//=================================================================================
+// WORKING FUNCTIONS
+//=================================================================================
+
+// getCompanyID();
+// addNewEmployee();
+//sendEmail();
+// viewEmployees();
+// isEmployeeExists();
+runProgram();
+
+
+//=================================================================================
+// INACTIVE FUNCTIONS
+//=================================================================================
+
+// addEmployeeToAllUsers();
+
+}
+// sendEmail();
+
+//===================================================
+// Tables Required: sponsor_user,all_users, company
+//===================================================
+
+
+//============================
+//List of functions:
+//============================
+
+function addNewEmployee(){ //Adds new employee to the database and sends email.
+
+  // Connection
+
+  $mysqli = new mysqli('localhost', 'root', '', 'math_u_fellows_all_updated_tables');
+
+
+  $role = $_POST['role_of_employee'];
+  $first_name = $_POST['fname'];
+  $last_name = $_POST['lname'];
+  $regdate = date('Y-m-d');
+  $email = strtolower($_POST['email']);
+  $sponsor_id = ucfirst($_POST['sponsor_id']);
+  $password = md5($first_name.$sponsor_id.$email);
+  $company_id = '';
+  $company_id = getCompanyID($_POST['company_name']);  //===================================================[[[[[Assign the Company ID within the add employee function -> Does not update automatically]]]]]]=============================
+  // echo "<br><br>Company ID: ".$company_id;
+
+  if($role == 'SuperAdmin'){
+
+  $sql = "INSERT INTO `sponsor_users` (`sponsor_id`, `first_name_of_user`, `last_name_of_user`, `email_address`, `password`, `regisered_date`,`company_id`, `isSuperAdmin`)
+  VALUES ('$sponsor_id', '$first_name', '$last_name', '$email', '$password', '$regdate','$company_id', 1)";
+  $entry = $mysqli->query($sql);
+
+} elseif ($role == 'manageBursaries'){
+
+$sql = "INSERT INTO `sponsor_users` (`sponsor_id`, `first_name_of_user`, `last_name_of_user`, `email_address`, `password`, `regisered_date`,`company_id`, `manageBursaries`)
+VALUES ('$sponsor_id', '$first_name', '$last_name', '$email', '$password', '$regdate','$company_id', 1)";
+$entry = $mysqli->query($sql);
+
+} else {
+$sql = "INSERT INTO `sponsor_users` (`sponsor_id`, `first_name_of_user`, `last_name_of_user`, `email_address`, `password`, `regisered_date`,`company_id`, `manageApplications`)
+VALUES ('$sponsor_id', '$first_name', '$last_name', '$email', '$password', '$regdate','$company_id', 1)";
+$entry = $mysqli->query($sql);
+
+}
+
+if($entry){
+  sendEmail();
+} else {
+  echo "Error Code: ".$mysqli->errno;
+}
+}
+
+//====================================================================================================
+
+//====================================================================================================
+
+function runProgram(){
+  isEmployeeExists();
+}
+
+//====================================================================================================
+
+function sendEmail(){
+
+  $mysqli = new mysqli('localhost', 'root', '', 'math_u_fellows_all_updated_tables');
+
+  $first_name = $_POST['fname'];
+  $email = strtolower($_POST['email']);
+  $sponsor_id = ucfirst($_POST['sponsor_id']);
+  $pword = $first_name.$sponsor_id.$email;
+
+  // Send Email
+
+  $to = $email;
+  $subject = 'Your account has been created';
+  // $message = "Record Added!";
+  $message = "Dear ".$first_name."\n\n"."Your account has been successfully created."."\n\n".
+              "Please login using you email address and dummy password: $pword"."\n\n".
+              "Please set a new password to verify your account."."\n\n".
+              "Thank you for using MathU Fellows."."\n\n"."Please do not reply to this email.";
+
+  if(mail($to,$subject,$message,'')) {
+    echo "<script>alert('Email sent');</script>";
+    // header("Location: view_employees.php");
+  } else {
+    echo "Failed to send email. Error Code: ".$mysqli->errno;
+  };
+
+}
+
+//====================================================================================================
+
+function viewEmployees(){
+    $mysqli = new mysqli('localhost', 'root', '', 'math_u_fellows_all_updated_tables');
+    $sponsor_id = ucfirst($_POST['sponsor_id']);
+    $sql = "SELECT `first_name_of_user`, `last_name_of_user`, `email_address` FROM `sponsor_users` WHERE `sponsor_id` = '$sponsor_id'";
+    $entry = $mysqli->query($sql);
+    if($entry){
+      $employees = array();
+      $p = 0;
+      while ($row = mysqli_fetch_array($entry, MYSQLI_ASSOC)) {
+        $employees[$p] = $row;
+        $p += 1;
+      }
+      print_r($employees);
+    } else {
+      echo "Could not view employees. Error Code: ".$mysqli->errno;
+    }
+}
+
+//====================================================================================================
+
+function getCompanyID() { //====================================================================================================[[[[[[Get Company ID function]]]]]]========================
+  // Connection
+  $mysqli = new mysqli('localhost', 'root', '', 'math_u_fellows_all_updated_tables');
+  if ($mysqli->connect_errno) {
+      echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+      }
+        // Get ID using Company Name
+        $company_name = $_POST['company_name'];
+        if($company_name){
+              $sql = "SELECT `company_id` FROM `company` WHERE `company_name` = '$company_name' LIMIT 1";
+                if($res1 = $mysqli->query($sql)){
+                  $res2 = mysqli_fetch_array($res1, MYSQLI_ASSOC);
+                  $res = print($res2['company_id']);
+
+          } else {
+            $res = "Query not processed".$mysqli->errno;
+          }
+
+      } else {
+        echo "Something went wrong. Company Name not found.";
+      }
+  return $res;
+  }
+
+  //====================================================================================================
+
+function isEmployeeExists(){
+  $mysqli = new mysqli('localhost', 'root', '', 'math_u_fellows_all_updated_tables');
+  $email = strtolower($_POST['email']);
+
+  //Checks if employee is already in sponsor_users, if not, employee gets Added
+
+  $dup = $mysqli->query("SELECT * FROM `sponsor_users` WHERE `email_address` = '$email'");
+  if(mysqli_num_rows($dup)==0){
+    addNewEmployee();
+    viewEmployees();
+  } else {
+  echo "Employee already exists";
+  }
+}
+
+//====================================================================================================
+
+//====================================================================================================
+
+//====================================================================================================
+
+ ?>
