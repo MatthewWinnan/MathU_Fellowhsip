c798ba1fa9ad0d568cb678361bbe547b27f9a52a
joy-mathe <61254406+joy-mathe@users.noreply.github.com>
2021-08-11 11:01:58 +0200

    Updated integrated back and frontend

--- backend/Coding Files/Integrated Back & Frontend/math_u_student_registration.php
+++ backend/Coding Files/Integrated Back & Frontend/math_u_student_registration.php
@@ -3,79 +3,51 @@
 include 'math_u_registration.php';
 include 'math_u_db_connection.php';
 
-$input = file_get_contents('php://input');
-$data = json_decode($input, true);
-$message = array();
 
+function AddStudent($email, $hashed_pass, $mysqli){
 
-$email = $data['email'];
-$pass = $data['password'];
-$isUnique = isUniqueEmail($email, $mysqli);
-$hashed_pass = GenerateHashPassword($pass);
+  $date = date("Y-m-d");
+  $sql = "INSERT INTO student (Email_address, Password, Registerred_date) VALUES ('$email', '$hashed_pass', '$date')";
 
-if ($isUnique === TRUE){
-    $student_id = AddStudent($email, $hashed_pass, $mysqli);
+  if($mysqli->query($sql)=== TRUE){
+		http_response_code(201);
+		return $mysqli->insert_id;
+	}
+	else{
+		http_response_code(422);
+		return 0;
+	}
+}
 
-    if ($student_id != 0){
-        $u_id = GenerateUserID($student_id, 1);
+function UpdateStudentID($student_id, $u_id, $mysqli){
 
-        if (UpdateStudentID($student_id, $u_id, $mysqli)===TRUE){
+  $sql = "UPDATE student SET Student_ID = '$u_id' WHERE ID = '".$student_id."'";
+  if($mysqli->query($sql)=== TRUE){
+		http_response_code(201);
+		return true;
+	}
+	else{
+		http_response_code(422);
+		return false;
+	}
+}
 
-          if (AddToUsers($email, $u_id, $mysqli) === TRUE){
-            echo json_encode("Success!");
-          }
-        }
-      }
-    }
-    else {
-      echo "This email address already exists";
-    }
-  }
+function display(int $student_id, $mysqli){
+	$sql = "SELECT (Email_address, Password) FROM student WHERE ID = $student_id";
+	$result = $mysqli->query($sql);
 
-  function AddStudent($email, $hashed_pass, $mysqli){
-
-    $date = date("Y-m-d");
-    $sql = "INSERT INTO student (Email_address, Password, Registerred_date) VALUES ('$email', '$hashed_pass', '$date')";
-
-    if($mysqli->query($sql)=== TRUE){
-  		http_response_code(201);
-  		return $mysqli->insert_id;
-  	}
-  	else{
-  		http_response_code(422);
-  		return 0;
-  	}
-  }
-
-  function UpdateStudentID($student_id, $u_id, $mysqli){
-
-    $sql = "UPDATE student SET Student_ID = '$u_id' WHERE ID = '".$student_id."'";
-    if($mysqli->query($sql)=== TRUE){
-  		http_response_code(201);
-  		return true;
-  	}
-  	else{
-  		http_response_code(422);
-  		return false;
-  	}
-  }
-
-  function display(int $student_id, $mysqli){
-  	$sql = "SELECT (Email_address, Password) FROM student WHERE ID = $student_id";
-  	$result = $mysqli->query($sql);
-
-  	if ($result->num_rows>0){
-  		http_response_code(201);
-  		$row = $result->fetch_assoc();
-  		$Obj=new \stdClass();
-  		$Obj->email = $row["email_address"];
-  		$Obj->password = $row["Password"];
-  		return $Obj;
-  	}
-  	else{
-  		http_response_code(422);
-  		return null;
-  	}
-  }
+	if ($result->num_rows>0){
+		http_response_code(201);
+		$row = $result->fetch_assoc();
+		$Obj=new \stdClass();
+		$Obj->email = $row["email_address"];
+		$Obj->password = $row["Password"];
+		return $Obj;
+	}
+	else{
+		http_response_code(422);
+		return null;
+	}
+}
 
  ?>
