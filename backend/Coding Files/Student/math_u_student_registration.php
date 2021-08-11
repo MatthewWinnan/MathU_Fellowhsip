0d47e2ed6c66e27307320f28f2eb872f28e56aaf
joy-mathe <61254406+joy-mathe@users.noreply.github.com>
2021-08-11 10:44:15 +0200

    Student registration

--- backend/Coding Files/Student/math_u_student_registration.php
+++ backend/Coding Files/Student/math_u_student_registration.php
@@ -3,35 +3,6 @@
 include_once 'math_u_registration.php';
 include_once 'math_u_db_connection.php';
 
-if(isset($_POST['email']) and isset($_POST['password'])){
-
-  $email = $_POST['email'];
-  $pass = $_POST['password'];
-  $isUnique = isUniqueEmail($email, $mysqli);
-  $hashed_pass = GenerateHashPassword($pass);
-
-  if ($isUnique === TRUE){
-      $student_id = AddStudent($email, $hashed_pass, $mysqli);
-
-      if ($student_id != 0){
-          $u_id = GenerateUserID($student_id, 1);
-
-          if (UpdateStudentID($student_id, $u_id, $mysqli)===TRUE){
-
-            if (AddToUsers($email, $u_id, $mysqli) === TRUE){
-              echo "New student added successfully";
-              echo "<script>window.location.href='student_input_test_page1.html'</script>";
-              return $student_id;
-            }
-          }
-        }
-      }
-      else {
-        echo "This email address already exists";
-      }
-    }
-
-
 
 function AddStudent($email, $hashed_pass, $mysqli){
 
