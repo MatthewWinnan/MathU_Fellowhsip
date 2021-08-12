6bab520b0aac27158da1303241a8f6faf8407ecf
Joshua1 <u19204915@tuks.co.za>
2021-08-12 16:49:26 +0200

    updated view-employee

--- frontend/ionicMathU/src/app/page/sponsor/view-employee/view-employee.page.ts
+++ frontend/ionicMathU/src/app/page/sponsor/view-employee/view-employee.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { AddNewEmployeePage } from '../add-new-employee/add-new-employee.page';
+
 
 @Component({
   selector: 'app-view-employee',
@@ -8,7 +11,43 @@ import {Router } from '@angular/router';
 })
 export class ViewEmployeePage implements OnInit {
 
-  constructor(private router: Router) { }
+  FirstName: string = 'First Name';
+
+  SuperAdminDetails = [{
+    firstName : 'Dan',
+    lastName : 'James',
+    adress : 'Danjames@gmail.com',
+  }]
+
+  EmployeeDetails = [{
+    FirstName : 'Joshua',
+    LastName : 'Akinsola',
+    Adress: 'jakinsola@gmail.com',
+    Rights: 'Manage Bursary/Manage Applications',
+  },
+  {
+    FirstName : 'Matt',
+    LastName : 'Johnson',
+    Adress: 'Matt@gmail.com',
+    Rights: 'Manage Bursary/Manage Applications',
+  },
+  {
+    FirstName : 'Josh',
+    LastName : 'Luus',
+    Adress: 'JoshLu@gmail.com',
+    Rights: 'Manage Bursary/Manage Applications',
+  },
+  {
+    FirstName : 'Raaga',
+    LastName : 'N',
+    Adress: 'Raaga@gmail.com',
+    Rights: 'Manage Bursary/Manage Applications',
+  },
+]
+
+today: number = Date.now()
+
+  constructor(private router: Router, public ModalCtrl: ModalController) { }
 
   ngOnInit() {
   }
@@ -18,5 +57,14 @@ export class ViewEmployeePage implements OnInit {
     this.router.navigate(['./../edit-employee']);
 
   }
+
+  async addEmployee() {
+    const modal = await this.ModalCtrl.create({
+      component : AddNewEmployeePage 
+    })
+
+    return await modal.present()
+  }
+ 
 
 }
