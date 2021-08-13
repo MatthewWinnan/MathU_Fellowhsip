cd3d7a328b9bc97f2314565b58990f2cebbd43fc
BenisMulenda <benismulenda54@gmail.com>
2021-08-13 15:43:55 +0200

    Update math_u_addStudent.php

--- backend/Coding Files/All User (Main Directory)/registration/math_u_addStudent.php
+++ backend/Coding Files/All User (Main Directory)/registration/math_u_addStudent.php
@@ -2,6 +2,7 @@
 
 include 'math_u_db_connection.php';
 include 'math_u_registration_functions.php';
+include 'all_classes.php';
 
 if(isset($_POST['email']) and isset($_POST['password']) and isset($_POST['first_name']) and isset($_POST['last_name'])){
 
@@ -11,7 +12,8 @@ if(isset($_POST['email']) and isset($_POST['password']) and isset($_POST['first_
   $last_name = $_POST['last_name'];
   $isUnique = isUniqueEmail($email, $mysqli);
   $hashed_pass = GenerateHashPassword($pass);
-
+  
+  $new_user = new all_users();
   if ($isUnique === TRUE){
 
       $student_id = AddStudent($first_name, $last_name, $email, $hashed_pass, $mysqli);
@@ -21,23 +23,24 @@ if(isset($_POST['email']) and isset($_POST['password']) and isset($_POST['first_
   		if (UpdateStudentID($student_id, $u_id, $mysqli)===TRUE){
 
   		  if (AddToUsers($email, $u_id, $mysqli) === TRUE){
-          $message = array();
-          $message["message"] = "Success!";
-          echo json_encode($message);
+          
+          $new_user->message = "Success! Your Account has been created. Check your email to verify account.";
+          echo json_encode($new_user);
   		  }
         else {
-          $message["message"] = "Could not account add to all users";
-          echo json_encode($message);
+			
+          $new_user->message= "ERROR CONNECTION TO SERVER (ALL USERS) FAILED";
+          echo json_encode($new_user);
         }
-  		}
+  	    }
       else {
-        $message["message"] = "Could not update student ID";
-        echo json_encode($message);
+        $new_user->message= "ERROR CONNECTION TO SERVER (STUDENT) FAILED";
+        echo json_encode($new_user);
       }
     }
     else {
-      $message["message"] = "Email address already exists";
-      echo json_encode($message);
+      $new_user->message = "Email address already exists!";
+      echo json_encode($new_user);
     }
   }
 
