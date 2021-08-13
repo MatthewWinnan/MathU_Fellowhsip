dd86867a683111cf13a5a0e32f101e3902e425ca
vraagakrishna <vraagakrishna@gmail.com>
2021-08-13 11:09:40 +0200

    imported Storage module

--- frontend/ionicMathU/src/app/app.module.ts
+++ frontend/ionicMathU/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SuperTabsModule } from '@ionic-super-tabs/angular';
 import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
     IonicModule.forRoot(), 
     HttpClientModule,
     AppRoutingModule, 
-    SuperTabsModule.forRoot()
+    SuperTabsModule.forRoot(),
+    IonicStorageModule.forRoot()
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
