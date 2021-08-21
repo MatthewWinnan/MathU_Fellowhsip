516f7e94fd4bc1363ae9269046280567b9a29c5a
vraagakrishna <vraagakrishna@gmail.com>
2021-08-21 23:56:25 +0200

    API calls test with dummy website created

--- frontend/ionicMathU/src/app/service/login-api.service.ts
+++ frontend/ionicMathU/src/app/service/login-api.service.ts
@@ -11,11 +11,12 @@ export class LoginApiService {
     this.headers = new HttpHeaders();
     this.headers.append("Accept", "application/json");
     this.headers.append("Content-Type", "application/json");
-    this.headers.append("Access-Control-Allow-Origin", "http://localhost:8100");
+    //this.headers.append("Access-Control-Allow-Origin", "http://localhost:8100");
+    this.headers.append("Access-Control-Allow-Origin", "*");
   }
 
   login_a_user(data:any){
-    return this.http.post<any>('http://localhost/mathU_backend/math_u_login.php', data);
-    //change the file name!
+    //return this.http.post<any>('http://localhost/mathU_backend/math_u_login.php', data);
+    return this.http.post<any>('https://mathufellows.epizy.com/math_u_login.php', data);
   }
 }
