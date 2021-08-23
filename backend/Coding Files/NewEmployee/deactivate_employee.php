b0c9681447aed07cf917884b052fa21fb07f73a3
NongSK <nongsydney59@gmail.com>
2021-08-23 18:21:30 +0200

    deactivate employee

---
+++ backend/Coding Files/NewEmployee/deactivate_employee.php
@@ -0,0 +1,44 @@
+<?php
+
+function deactivateEmployee(){
+
+  include 'math_u_db_connection.php';
+
+  $input = file_get_contents('php://input');
+  $data = json_decode($input, true);
+
+  $sponsor_id = $data['sponsor_id'];
+  $company_id = $data['company_id'];
+  $email = $data['email_address'];
+
+  class deactivateEmployee{
+    public $message;
+  }
+
+  $sql = "UPDATE `sponsor_users` SET `inactive`= 1
+          WHERE `sponsor_id`='$sponsor_id' AND `company_id`='$company_id' AND 'email_address' = '$email' ";
+  $entry = $mysqli->query($sql);
+
+  if($entry){
+    $deactivate = new deactivateEmployee();
+    $deactivate->message = "Employee Deactivated";
+
+    // echo json_encode($deactivate);
+
+    return json_encode($deactivate);
+
+  } else {
+    $deactivate = new deactivateEmployee();
+    $deactivate->message = "Failed to deactivate employee.";
+
+    // echo $mysqli->errno;
+    
+    // echo json_encode($deactivate);
+
+    return json_encode($deactivate);
+  }
+}
+
+deactivateEmployee();
+
+ ?>
