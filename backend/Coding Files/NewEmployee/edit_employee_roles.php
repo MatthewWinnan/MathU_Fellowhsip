d3141fba210b527fcf55b2059c28fa4ba102b843
NongSK <nongsydney59@gmail.com>
2021-08-23 17:26:40 +0200

    edit employee role

---
+++ backend/Coding Files/NewEmployee/edit_employee_roles.php
@@ -0,0 +1,70 @@
+<?php
+
+function editEmployeeRole(){
+
+  // Connection
+
+  $mysqli = new mysqli('localhost', 'root', '', 'math_u_fellows');
+
+  // included in case code does not run
+  $input = file_get_contents('php://input');
+  $data = json_decode($input, true);
+
+  class Role{
+    public $message;
+  }
+
+  $sponsor_id = ucfirst($data['sponsor_id']);
+  $company_id = $data['company_id'];
+  $isSuperAdmin = $data['isSuperAdmin'];
+  $manageBursaries = $data['manageBursaries'];
+  $manageApplications = $data['manageApplications'];
+
+  // Update database.
+
+  if($isSuperAdmin === 'true'){
+
+  $sql = "UPDATE `sponsor_users` SET `isSuperAdmin`= 1, `manageBursaries`= 1, `manageApplications`= 1
+          WHERE `sponsor_id`='$sponsor_id' AND `company_id`='$company_id' ";
+  $entry = $mysqli->query($sql);
+
+} elseif ($manageBursaries === 'true' AND $manageApplications === 'false' AND $isSuperAdmin === 'false'){
+
+  $sql = "UPDATE `sponsor_users` SET `isSuperAdmin`= 0, `manageBursaries`= 1, `manageApplications`= 0
+          WHERE `sponsor_id`='$sponsor_id' AND `company_id`='$company_id' ";
+ $entry = $mysqli->query($sql);
+
+} elseif ($manageBursaries === 'false' AND $manageApplications === 'true' AND $isSuperAdmin === 'false'){
+  $sql = "UPDATE `sponsor_users` SET `isSuperAdmin`= 0, `manageBursaries`= 0, `manageApplications`= 1
+          WHERE `sponsor_id`='$sponsor_id' AND `company_id`='$company_id' ";
+ $entry = $mysqli->query($sql);
+
+
+  } else {
+    $sql = "UPDATE `sponsor_users` SET `isSuperAdmin`= 0, `manageBursaries`= 1, `manageApplications`= 1
+    WHERE `sponsor_id`='$sponsor_id' AND `company_id`='$company_id' ";
+    $entry = $mysqli->query($sql);
+    echo "both";
+
+  }
+
+  if($entry){
+    $updateRole = new Role();
+    $updateRole->message = "Role Updated.";
+
+    echo json_encode($updateRole);
+
+    return json_encode($updateRole);
+
+  } else {
+    $updateRole = new Role();
+    $updateRole->message = "Failed updating employee role.";
+
+    echo json_encode($updateRole);
+
+    return json_encode($updateRole);
+  }
+}
+
+editEmployeeRole();
+ ?>
