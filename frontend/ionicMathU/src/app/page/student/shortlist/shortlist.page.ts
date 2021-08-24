fedf1ea2ba700aa19fe4fad9c3d0120b69b85ed1
Josh <luus.josh@gmail.com>
2021-08-24 16:19:38 +0200

    Made the Shortlist Page Neeter and less distracting

--- frontend/ionicMathU/src/app/page/student/shortlist/shortlist.page.ts
+++ frontend/ionicMathU/src/app/page/student/shortlist/shortlist.page.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Company } from 'src/app/model/company';
+
+let company = new Company
 
 @Component({
   selector: 'app-shortlist',
   templateUrl: './shortlist.page.html',
   styleUrls: ['./shortlist.page.scss'],
 })
+
 export class ShortlistPage implements OnInit {
 
   bursaryShortlist = [
-    {bursName: "MechEng", bursCompName: "Sasol"},
-    {bursName: "CivEng", bursCompName: "Hatch"}
+    {bursName: "MechEng", bursCompName: "Sasol", bursID: 0, bursIcon: company.comapny_logo, bursStatus: "Pending"},
+    {bursName: "CivEng", bursCompName: "Hatch", bursID: 1, bursIcon: company.comapny_logo, bursStatus: "Accepted" }
   ]
 
   constructor() { }
@@ -18,4 +23,16 @@ export class ShortlistPage implements OnInit {
   ngOnInit() {
   }
 
-}
+  goToBursaryView(bursID) {
+    console.log("goToBursaryView() ran with the ",  bursID )
+
+  }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }}
