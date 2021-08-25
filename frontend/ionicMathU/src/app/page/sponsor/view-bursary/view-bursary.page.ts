3836d14ed8c9b3c743ade7339a0e669e6adba4ce
vraagakrishna <vraagakrishna@gmail.com>
2021-08-25 13:29:47 +0200

    view bursary with backend (WIP)

--- frontend/ionicMathU/src/app/page/sponsor/view-bursary/view-bursary.page.ts
+++ frontend/ionicMathU/src/app/page/sponsor/view-bursary/view-bursary.page.ts
@@ -43,107 +43,109 @@ export class ViewBursaryPage implements OnInit {
   }
 
   initializeJSONData() {
-    this.jsonData = [
-      {
-        "company_id": 0,
-        "bursary_name": "Retirement",
-        "bursary_type": "Work Back",
-        "bursary_description": "This is a good bursary",
-        "WB_duration": 4,
-        "min_age": 13,
-        "max_age": 26,
-        "academic_level": "Undergraduate",
-        "study_field": "Law",
-        "minimum_year_required": 2,
-        "min_average": 76,
-        "RSA_citizen": true,
-        "financial_need": false,
-        "study_further": false,
-        "disability": true,
-        "province": "Gauteng",
-        "bursary_covers": [
-            "Accommodation Fees",
-            "Meals",
-            "Books Allowance",
-            "Transport"
-        ],
-        "closing_date": "2021-11-16",
-        "shortlist_date": "2021-11-20",
-        "email_address": "name@gmail.com",
-        "bursary_duration": 2,
-        "isVisible": true
-      },
-      {
-        "company_id": 0,
-        "bursary_name": "Amazing",
-        "bursary_type": "Bursary",
-        "bursary_description": "This is an amazing bursary",
-        "WB_duration": 0,
-        "min_age": 20,
-        "max_age": 23,
-        "academic_level": "Undergraduate",
-        "study_field": "Law",
-        "minimum_year_required": 2,
-        "min_average": 76,
-        "RSA_citizen": true,
-        "financial_need": true,
-        "study_further": false,
-        "disability": false,
-        "province": "Gauteng",
-        "bursary_covers": [
-            "Accommodation Fees",
-            "Books Allowance",
-            "Transport"
-        ],
-        "closing_date": "2021-11-16",
-        "shortlist_date": "2021-11-20",
-        "email_address": "name@gmail.com",
-        "bursary_duration": 2,
-        "isVisible": true
-      },
-      {
-        "company_id": 0,
-        "bursary_name": "Help out",
-        "bursary_type": "Full Bursary",
-        "bursary_description": "This is a bursary",
-        "WB_duration": 0,
-        "min_age": 13,
-        "max_age": 26,
-        "academic_level": "High School",
-        "study_field": "",
-        "minimum_year_required": 0,
-        "min_average": 76,
-        "RSA_citizen": true,
-        "financial_need": false,
-        "study_further": true,
-        "disability": false,
-        "province": "Gauteng",
-        "bursary_covers": [
-            "Accommodation Fees",
-            "Meals",
-            "Books Allowance",
-        ],
-        "closing_date": "2021-12-16",
-        "shortlist_date": "2021-12-20",
-        "email_address": "name@gmail.com",
-        "bursary_duration": 2,
-        "isVisible": true
-      }
-    ];
-    this.jsonData_length = this.jsonData.length;
-    // this.isFetching = true;
-    // // all bursaries with company_id that are 
-    // this._apiService.getAllBursary(this.ourCompany).subscribe((res:Bursary[]) => {
-    //   console.log("REQUEST SUCCESS ===", res);
-    //   this.jsonData = res;
-    //   if(res!=null){
-    //     this.jsonData_length = this.jsonData.length;
+    // this.jsonData = [
+    //   {
+    //     "company_id": 0,
+    //     "bursary_name": "Retirement",
+    //     "bursary_type": "Work Back",
+    //     "bursary_description": "This is a good bursary",
+    //     "WB_duration": 4,
+    //     "min_age": 13,
+    //     "max_age": 26,
+    //     "academic_level": "Undergraduate",
+    //     "study_field": "Law",
+    //     "minimum_year_required": 2,
+    //     "min_average": 76,
+    //     "RSA_citizen": true,
+    //     "financial_need": false,
+    //     "study_further": false,
+    //     "disability": true,
+    //     "province": "Gauteng",
+    //     "bursary_covers": [
+    //         "Accommodation Fees",
+    //         "Meals",
+    //         "Books Allowance",
+    //         "Transport"
+    //     ],
+    //     "closing_date": "2021-11-16",
+    //     "shortlist_date": "2021-11-20",
+    //     "email_address": "name@gmail.com",
+    //     "bursary_duration": 2,
+    //     "isVisible": true
+    //   },
+    //   {
+    //     "company_id": 0,
+    //     "bursary_name": "Amazing",
+    //     "bursary_type": "Bursary",
+    //     "bursary_description": "This is an amazing bursary",
+    //     "WB_duration": 0,
+    //     "min_age": 20,
+    //     "max_age": 23,
+    //     "academic_level": "Undergraduate",
+    //     "study_field": "Law",
+    //     "minimum_year_required": 2,
+    //     "min_average": 76,
+    //     "RSA_citizen": true,
+    //     "financial_need": true,
+    //     "study_further": false,
+    //     "disability": false,
+    //     "province": "Gauteng",
+    //     "bursary_covers": [
+    //         "Accommodation Fees",
+    //         "Books Allowance",
+    //         "Transport"
+    //     ],
+    //     "closing_date": "2021-11-16",
+    //     "shortlist_date": "2021-11-20",
+    //     "email_address": "name@gmail.com",
+    //     "bursary_duration": 2,
+    //     "isVisible": true
+    //   },
+    //   {
+    //     "company_id": 0,
+    //     "bursary_name": "Help out",
+    //     "bursary_type": "Full Bursary",
+    //     "bursary_description": "This is a bursary",
+    //     "WB_duration": 0,
+    //     "min_age": 13,
+    //     "max_age": 26,
+    //     "academic_level": "High School",
+    //     "study_field": "",
+    //     "minimum_year_required": 0,
+    //     "min_average": 76,
+    //     "RSA_citizen": true,
+    //     "financial_need": false,
+    //     "study_further": true,
+    //     "disability": false,
+    //     "province": "Gauteng",
+    //     "bursary_covers": [
+    //         "Accommodation Fees",
+    //         "Meals",
+    //         "Books Allowance",
+    //     ],
+    //     "closing_date": "2021-12-16",
+    //     "shortlist_date": "2021-12-20",
+    //     "email_address": "name@gmail.com",
+    //     "bursary_duration": 2,
+    //     "isVisible": true
     //   }
-    //   this.isFetching = false;
-    // }, (error:any) => {
-    //   console.log("ERROR ===", error);
-    //   this.jsonData = [];
-    // });
+    // ];
+    // this.jsonData_length = this.jsonData.length;
+
+    this.isFetching = true;
+    console.log(this.ourCompany);
+    // all bursaries with company_id that are 
+    this._apiService.getAllBursary(this.ourCompany).subscribe((res:Bursary[]) => {
+      console.log("REQUEST SUCCESS ===", res);
+      this.jsonData = res;
+      if(res!=null){
+        this.jsonData_length = this.jsonData.length;
+      }
+      this.isFetching = false;
+    }, (error:any) => {
+      console.log("ERROR ===", error);
+      this.jsonData = [];
+    });
   }
 
   filterBursary(ev:any) {
