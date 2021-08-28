eeddc68dc4c4ef62cbfdfa4663f65cbfb2c5ca79
vraagakrishna <vraagakrishna@gmail.com>
2021-08-28 21:30:31 +0200

    alterations to edit student profile form

--- frontend/ionicMathU/src/app/page/student/student-edit-profile/student-edit-profile.page.ts
+++ frontend/ionicMathU/src/app/page/student/student-edit-profile/student-edit-profile.page.ts
@@ -15,8 +15,7 @@ export class ViewProfilePage implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private router:Router,
-
-    ) {
+  ) {
       //get data from storage 
       //for now creating a dummy dataset
       this.thisStudent.student_id = "U0001";
@@ -46,14 +45,14 @@ export class ViewProfilePage implements OnInit {
       this.thisStudent.website = "";
       this.thisStudent.Students_marks = [];
 
-     }
+  }
 
   addStudent_details = this.formBuilder.group({
     contact_number: ['',[Validators.required, Validators.pattern("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$")]],
-    city: [''],
-    province: [''],
-    // RSA_citizen: [false],
+    rsa_citizen: [false],
     disability: [false],
+    city: [''],
+    province: [''],    
     current_academic_level: [''],
     grade: [0],
     syllabus: [''],
@@ -62,37 +61,39 @@ export class ViewProfilePage implements OnInit {
     year_of_study: [''],
     study_institution: [''],
     continue_studies: [false],
-    gpa: [0.0],
+    gpa: [60.0,[Validators.min(60)]],
     description_of_student: [''],
+    website: [''],
     bursarred: [''],
-    workback: [''],
     current_bursaries: [''],
+    workback: [''],
+    
   });
 
   //Gets for all the form's parameters
   get contact_number() {
     return this.addStudent_details.get('contact_number');
   }
+  get rsa_citizen() {
+    return this.addStudent_details.get('rsa_citizen');
+  }
   get disability() {
     return this.addStudent_details.get('disability');
+  }
+  get city() {
+    return this.addStudent_details.get('city');
   }
   get province() {
     return this.addStudent_details.get('province');
   }
-  // get RSA_citizen() {
-  //   return this.addStudent_details.get('RSA_citizen');
-  // }
-  get city() {
-    return this.addStudent_details.get('city');
-  }
   get current_academic_level() {
     return this.addStudent_details.get('current_academic_level');
   }
-  get syllabus() {
-    return this.addStudent_details.get('syllabus');
-  }
   get grade() {
     return this.addStudent_details.get('grade');
+  }
+  get syllabus() {
+    return this.addStudent_details.get('syllabus');
   }
   get average() {
     return this.addStudent_details.get('average');
@@ -106,24 +107,28 @@ export class ViewProfilePage implements OnInit {
   get study_institution() {
     return this.addStudent_details.get('study_institution');
   }
-  // get disability() {
-  //   return this.addStudent_details.get('disability');
-  // }
+  get continue_studies() {
+    return this.addStudent_details.get('continue_studies');
+  }
   get gpa() {
     return this.addStudent_details.get('gpa');
   }
   get description_of_student() {
     return this.addStudent_details.get('description_of_student');
   }
+  get website() {
+    return this.addStudent_details.get('website');
+  }
   get bursarred() {
     return this.addStudent_details.get('bursarred');
-  }
-  get workback() {
-    return this.addStudent_details.get('workback');
   }
   get current_bursaries() {
     return this.addStudent_details.get('current_bursaries');
   }
+  get workback() {
+    return this.addStudent_details.get('workback');
+  }
+  
 
   public errorMessages = {
     contact_number: [
@@ -134,11 +139,15 @@ export class ViewProfilePage implements OnInit {
     average: [
       {type: 'min', message:'Average should atleast be 60'},
     ],
+    gpa: [
+      {type: 'min', message:'Average should atleast be 60'},
+    ],
   };
 
   public addStudentDetails() {
     console.log(this.addStudent_details.value);
     this.thisStudent.contact_number = this.addStudent_details.value.contact_number;
+    this.thisStudent.nationality = this.addStudent_details.value.rsa_citizen;
     this.thisStudent.disability = this.addStudent_details.value.disability;
     this.thisStudent.city = this.addStudent_details.value.city;
     this.thisStudent.province = this.addStudent_details.value.province;
@@ -149,15 +158,15 @@ export class ViewProfilePage implements OnInit {
     this.thisStudent.currently_studying = this.addStudent_details.value.currently_studying;
     this.thisStudent.year_of_study = this.addStudent_details.value.year_of_study;
     this.thisStudent.study_institution = this.addStudent_details.value.study_institution;
+    this.thisStudent.continue_studies = this.addStudent_details.value.continue_studies;
     this.thisStudent.gpa = this.addStudent_details.value.gpa;
     this.thisStudent.description_of_student = this.addStudent_details.value.description_of_student;
+    this.thisStudent.website = this.addStudent_details.value.website;
     this.thisStudent.bursarred = this.addStudent_details.value.bursarred;
-    this.thisStudent.workback = this.addStudent_details.value.workback;
     this.thisStudent.current_bursaries = this.addStudent_details.value.current_bursaries ;
-
-    //do for all fields 
-
-    //console.log(this.student);
+    this.thisStudent.workback = this.addStudent_details.value.workback;
+    // add in marks
+    console.log(this.thisStudent);
     //this.router.navigate(['./student-view-profile']);
   }
 
@@ -169,24 +178,24 @@ export class ViewProfilePage implements OnInit {
           Validators.pattern("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$")
         ]
       ],
+      rsa_citizen: [this.thisStudent.nationality],
       disability: [this.thisStudent.disability],
-      city: [''],
-      province: [''],
-      // RSA_citizen: [false],
-      // disability: [false],
-      current_academic_level: [''],
-      grade: [0],
-      syllabus: [''],
-      average: [60.0,[Validators.min(60)]],
-      currently_studying: [''],
-      year_of_study: [''],
-      study_institution: [''],
-      continue_studies: [false],
-      gpa: [0.0],
-      description_of_student: [''],
-      bursarred: [false],
-      current_bursaries: [''],
-      workback: [false],
+      city: [this.thisStudent.city],
+      province: [this.thisStudent.province],
+      current_academic_level: [this.thisStudent.current_academic_level],
+      grade: [this.thisStudent.grade],
+      syllabus: [this.thisStudent.syllabus],
+      average: [this.thisStudent.average,[Validators.min(60)]],
+      currently_studying: [this.thisStudent.currently_studying],
+      year_of_study: [this.thisStudent.year_of_study],
+      study_institution: [this.thisStudent.study_institution],
+      continue_studies: [this.thisStudent.continue_studies],
+      gpa: [this.thisStudent.gpa],
+      description_of_student: [this.thisStudent.description_of_student],
+      website: [this.thisStudent.website],
+      bursarred: [this.thisStudent.bursarred],
+      current_bursaries: [this.thisStudent.current_bursaries],
+      workback: [this.thisStudent.workback],
     });
   }
 
