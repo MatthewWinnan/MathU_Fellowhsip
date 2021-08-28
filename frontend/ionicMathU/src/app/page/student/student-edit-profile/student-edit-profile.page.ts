507b5236cd36a023367004ae841b7230535ad5c9
Joshua1 <u19204915@tuks.co.za>
2021-08-28 20:42:04 +0200

    added in edit profile students. current bursary check missing

--- frontend/ionicMathU/src/app/page/student/student-edit-profile/student-edit-profile.page.ts
+++ frontend/ionicMathU/src/app/page/student/student-edit-profile/student-edit-profile.page.ts
@@ -50,20 +50,23 @@ export class ViewProfilePage implements OnInit {
 
   addStudent_details = this.formBuilder.group({
     contact_number: ['',[Validators.required, Validators.pattern("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$")]],
-    // city: [''],
-    // province: [''],
+    city: [''],
+    province: [''],
     // RSA_citizen: [false],
     disability: [false],
-    // current_academic_level: [''],
-    // grade: [0],
-    // syllabus: [''],
-    // average: [60.0,[Validators.min(60)]],
-    // currently_studying: [''],
-    // year_of_study: [''],
-    // study_institution: [''],
-    // continue_studies: [false],
-    // gpa: [0.0],
-    // description_of_student: [''],
+    current_academic_level: [''],
+    grade: [0],
+    syllabus: [''],
+    average: [60.0,[Validators.min(60)]],
+    currently_studying: [''],
+    year_of_study: [''],
+    study_institution: [''],
+    continue_studies: [false],
+    gpa: [0.0],
+    description_of_student: [''],
+    bursarred: [''],
+    workback: [''],
+    current_bursaries: [''],
   });
 
   //Gets for all the form's parameters
@@ -73,42 +76,54 @@ export class ViewProfilePage implements OnInit {
   get disability() {
     return this.addStudent_details.get('disability');
   }
-  // get province() {
-  //   return this.addStudent_details.get('province');
-  // }
+  get province() {
+    return this.addStudent_details.get('province');
+  }
   // get RSA_citizen() {
   //   return this.addStudent_details.get('RSA_citizen');
   // }
-  // get city() {
-  //   return this.addStudent_details.get('city');
-  // }
-  // get current_academic_level() {
-  //   return this.addStudent_details.get('current_academic_level');
-  // }
-  // get syllabus() {
-  //   return this.addStudent_details.get('syllabus');
-  // }
-  // get grade() {
-  //   return this.addStudent_details.get('grade');
-  // }
-  // get average() {
-  //   return this.addStudent_details.get('average');
-  // }
-  // get currently_studying() {
-  //   return this.addStudent_details.get('currently_studying');
-  // }
-  // get year_of_study() {
-  //   return this.addStudent_details.get('year_of_study');
-  // }
-  // get study_institution() {
-  //   return this.addStudent_details.get('study_institution');
-  // }
+  get city() {
+    return this.addStudent_details.get('city');
+  }
+  get current_academic_level() {
+    return this.addStudent_details.get('current_academic_level');
+  }
+  get syllabus() {
+    return this.addStudent_details.get('syllabus');
+  }
+  get grade() {
+    return this.addStudent_details.get('grade');
+  }
+  get average() {
+    return this.addStudent_details.get('average');
+  }
+  get currently_studying() {
+    return this.addStudent_details.get('currently_studying');
+  }
+  get year_of_study() {
+    return this.addStudent_details.get('year_of_study');
+  }
+  get study_institution() {
+    return this.addStudent_details.get('study_institution');
+  }
   // get disability() {
   //   return this.addStudent_details.get('disability');
   // }
-  // get gpa() {
-  //   return this.addStudent_details.get('gpa');
-  // }
+  get gpa() {
+    return this.addStudent_details.get('gpa');
+  }
+  get description_of_student() {
+    return this.addStudent_details.get('description_of_student');
+  }
+  get bursarred() {
+    return this.addStudent_details.get('bursarred');
+  }
+  get workback() {
+    return this.addStudent_details.get('workback');
+  }
+  get current_bursaries() {
+    return this.addStudent_details.get('current_bursaries');
+  }
 
   public errorMessages = {
     contact_number: [
@@ -116,15 +131,30 @@ export class ViewProfilePage implements OnInit {
       {type: 'pattern', message:'Please enter valid Contact Number'},
     ],
     disability: [],
-    // average: [
-    //   {type: 'min', message:'Average should atleast be 60'},
-    // ],
+    average: [
+      {type: 'min', message:'Average should atleast be 60'},
+    ],
   };
 
   public addStudentDetails() {
     console.log(this.addStudent_details.value);
     this.thisStudent.contact_number = this.addStudent_details.value.contact_number;
     this.thisStudent.disability = this.addStudent_details.value.disability;
+    this.thisStudent.city = this.addStudent_details.value.city;
+    this.thisStudent.province = this.addStudent_details.value.province;
+    this.thisStudent.current_academic_level = this.addStudent_details.value.current_academic_level;
+    this.thisStudent.grade = this.addStudent_details.value.grade;
+    this.thisStudent.syllabus = this.addStudent_details.value.syllabus;
+    this.thisStudent.average = this.addStudent_details.value.average;
+    this.thisStudent.currently_studying = this.addStudent_details.value.currently_studying;
+    this.thisStudent.year_of_study = this.addStudent_details.value.year_of_study;
+    this.thisStudent.study_institution = this.addStudent_details.value.study_institution;
+    this.thisStudent.gpa = this.addStudent_details.value.gpa;
+    this.thisStudent.description_of_student = this.addStudent_details.value.description_of_student;
+    this.thisStudent.bursarred = this.addStudent_details.value.bursarred;
+    this.thisStudent.workback = this.addStudent_details.value.workback;
+    this.thisStudent.current_bursaries = this.addStudent_details.value.current_bursaries ;
+
     //do for all fields 
 
     //console.log(this.student);
@@ -140,20 +170,23 @@ export class ViewProfilePage implements OnInit {
         ]
       ],
       disability: [this.thisStudent.disability],
-      // city: [''],
-      // province: [''],
+      city: [''],
+      province: [''],
       // RSA_citizen: [false],
       // disability: [false],
-      // current_academic_level: [''],
-      // grade: [0],
-      // syllabus: [''],
-      // average: [60.0,[Validators.min(60)]],
-      // currently_studying: [''],
-      // year_of_study: [''],
-      // study_institution: [''],
-      // continue_studies: [false],
-      // gpa: [0.0],
-      // description_of_student: [''],
+      current_academic_level: [''],
+      grade: [0],
+      syllabus: [''],
+      average: [60.0,[Validators.min(60)]],
+      currently_studying: [''],
+      year_of_study: [''],
+      study_institution: [''],
+      continue_studies: [false],
+      gpa: [0.0],
+      description_of_student: [''],
+      bursarred: [false],
+      current_bursaries: [''],
+      workback: [false],
     });
   }
 
